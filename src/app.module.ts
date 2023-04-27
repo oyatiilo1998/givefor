76b1294344f20a4d12f9964572203057c29de620
Sardor Abdurakhimov <abdurakhimov.sardor@gmail.com>
2023-04-27 19:20:47 +0500

    database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import { typeOrmAsyncConfig } from './config/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
